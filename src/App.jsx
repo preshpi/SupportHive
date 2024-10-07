31346521e57e3b83e70896a110d8ee001cd64127
Uwakmfon-Ekong <uwakmfone470@gmail.com>
2024-10-07 20:09:55 +0100

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from '../views/home';
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+       
+      </Routes>
+    </Router>
+  );
 }
+
+export default App;
---
+++ views/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Login from '../src/components/Login';
+
+const Home = () => {
+  return (
+    <div>
+      <Login/>
+    </div>
+  );
+};
+
+export default Home;
