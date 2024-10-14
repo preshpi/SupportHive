335acc3923efc9f3f99b2562fbb750fae83e739e
Uwakmfon-Ekong <uwakmfone470@gmail.com>
2024-10-14 06:26:10 +0100

    create account and forgot password page done

--- src/Layout/LandingPageLayout.tsx
+++ src/Layout/LandingPageLayout.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Navbar from '../components/Navbar';
+// import React from 'react';
+// import { Outlet } from 'react-router-dom';
+// import Navbar from '../components/Navbar';
 
-const MainLayout = () => {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <main className="flex-grow container mx-auto p-4">
-        <Outlet />
-      </main>
+// const MainLayout = () => {
+//   return (
+//     <div className="flex flex-col min-h-screen">
+//       <main className="flex-grow container mx-auto p-4">
+//         <Outlet />
+//       </main>
      
-    </div>
-  );
-};
+//     </div>
+//   );
+// };
 
-export default MainLayout;
+// export default MainLayout;
