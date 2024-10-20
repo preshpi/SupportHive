3e92cf2aaf40e50fdba17495992023df3b19dbdc
Uwakmfon-Ekong <uwakmfone470@gmail.com>
2024-10-20 01:45:30 +0100

    contact info

--- src/components/Information/CampaignInformation.tsx
+++ src/components/Information/CampaignInformation.tsx
@@ -1,16 +1,32 @@
+import React, { useState } from 'react';
+import CampaignInput from "../CampaignInput";
 
-const CampaignInformation = () => {
+const CampaignInformation :React.FC =() => {
+  const [campaignTitle, setCampaignTitle] = useState('');
+  const [country, setCountry] = useState('');
+  const [city, setCity] = useState('');
+
+
   return (
-    <div>
-      <h3 className="text-xl mb-4">Campaign Information</h3>
-      <form>
-        <div className="mb-4">
-          <label className="block text-sm">Campaign Description</label>
-          <textarea className="w-full border p-2 rounded" placeholder="Enter Campaign Description"></textarea>
-        </div>
-
-        
-      </form>
+    <div className='w-[60%]'>
+      <CampaignInput
+        label="Campaign Description"
+        placeholder="Write Here"
+        value={campaignTitle}
+        onChange={(e) => setCampaignTitle(e.target.value)}
+      />
+      <CampaignInput
+        label="Country"
+        placeholder="Enter Country"
+        value={country}
+        onChange={(e) => setCountry(e.target.value)}
+      />
+      <CampaignInput
+        label="City"
+        placeholder="Enter City"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
     </div>
   );
 };
