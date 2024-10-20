1d265fb77c5706627437d06f8856facd617e8c69
Precious Egwuenu <ijeomaegwuenu22@gmail.com>
2024-10-20 09:17:17 +0100

    feat: campain creation

--- src/views/Dashboard/Campaigns.tsx
+++ src/views/Dashboard/Campaigns.tsx
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
-import Tabs from '../../UI/TabComponent/tabs';
-import BasicInformation from '../../components/Information/BasicInformation';
-import CampaignInformation from '../../components/Information/CampaignInformation';
-import ContactInformation from '../../components/Information/ContactInformation';
-import Creator from '../../components/campaign/Creator';
-
+import React, { useState } from "react";
+import Tabs from "../../UI/TabComponent/tabs";
+import BasicInformation from "../../components/Information/BasicInformation";
+import CampaignInformation from "../../components/Information/CampaignInformation";
+import ContactInformation from "../../components/Information/ContactInformation";
+import Creator from "../../components/campaign/Creator";
 
 const CampaignForm: React.FC = () => {
   const [activeTab, setActiveTab] = useState(0);
@@ -15,27 +14,26 @@ const CampaignForm: React.FC = () => {
 
   return (
     <div>
-      
       <Tabs
-        tabs={['Basic Information', 'Campaign Information', 'Contact Information']}
+        tabs={[
+          "Basic Information",
+          "Campaign Information",
+          "Contact Information",
+        ]}
         activeTab={activeTab}
         onTabChange={handleTabChange}
       />
 
-      
       <div className="mt-6">
         {activeTab === 0 && <BasicInformation />}
         {activeTab === 1 && <CampaignInformation />}
         {activeTab === 2 && <ContactInformation />}
-       </div>
-
-       <div>
-       <Creator />
-       </div>
-      
-
       </div>
-    
+
+      <div>
+        <Creator />
+      </div>
+    </div>
   );
 };
 
