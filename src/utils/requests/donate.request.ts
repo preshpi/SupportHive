e0951dcac2e41ced579946e29e5cf37a86f17988
Precious Egwuenu <ijeomaegwuenu22@gmail.com>
2024-10-24 13:46:48 +0100

    fix/callback link

---
+++ src/utils/requests/donate.request.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { donateSchema } from "../../types/donate";
+import { toast } from "sonner";
+
+export const handlePaymentInitialization = async (
+  campaignData: donateSchema
+) => {
+  const url = "https://api.paystack.co/transaction/initialize";
+  const headers = {
+    Authorization: `Bearer ${process.env.VITE_PAYSTACK_SECRET_KEY}`,
+    "Content-Type": "application/json",
+  };
+
+  const data = {
+    email: campaignData.email,
+    amount: campaignData.amount * 100,
+    subaccount: campaignData.subAccountId,
+    transaction_charge: 100,
+    bearer: "subaccount",
+    callback_url: "https://support-hive.vercel.app/dashboard/transactions",
+    metadata: {
+      userId: campaignData.userId,
+      campaignId: campaignData.campaignId,
+    },
+  };
+
+  try {
+    const response = await axios.post(url, data, { headers });
+    const { authorization_url } = response.data.data;
+
+    window.location.href = authorization_url;
+  } catch (error) {
+    toast.error((error as { message: string }).message);
+  }
+};
